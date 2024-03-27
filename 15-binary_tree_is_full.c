64b014b5f8dd080aa9ee2739a2b0b10f32c97597
Victor <vc.anokwuru@gmail.com>
2024-03-27 06:08:10 -0700

    betty code style correction

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_full - checks if a binary tree is full
- * @tree: a pointer to the root node of the tree 
+ * @tree: a pointer to the root node of the tree
  * Return: 1 if full or 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
