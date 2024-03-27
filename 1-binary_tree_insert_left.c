d4000bae48da1e5d61a4bc3cc2ebce431a67cb53
Victor <vc.anokwuru@gmail.com>
2024-03-27 10:18:21 -0700

    correct code not to count root node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,19 +8,15 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tmp;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
-	{
-		tmp = parent->left;
-		parent->left = binary_tree_node(parent, value);
-		parent->left->left = tmp;
-	}
-	else
-		parent->left = binary_tree_node(parent, value);
+		new_node->left = parent->left;
+	parent->left = new_node;
 
 	return (parent->left);
 }
